1540cedea88dc4fecbf74b630e32144a25dda824
이기태 <91439717+kitaee@users.noreply.github.com>
2023-08-24 21:59:41 +0900

    Create 121. Best Time to Buy and Sell Stock.java
    🔗 문제 링크
    https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/?envType=study-plan-v2&envId=top-interview-150 
    
    Best Time to Buy and Sell Stock - LeetCode
    
    Can you solve this real interview question? Best Time to Buy and Sell Stock - You are given an array prices where prices[i] is the price of a given stock on the ith day. You want to maximize your profit by choosing a single day to buy one stock and choosin
    
    leetcode.com
    📝 문제 설명
    가격 배열 prices가 주어졌을 때, 언제 사고 팔아야 최대의 이익을 낼 수 있는지에 대해 구하는 문제이다.
    
    당연하게도 사기 전에는 팔 수 없으며 단 한번만 사고 팔 수 있다.
    
    
    
    
    🤔 접근 방식
    문제의 조건을 보자. 가격 배열 prices은 10^5까지의 길이인데, 느낌 상 O(n^2)으로 풀면 시간초과가 나올 듯하다.﻿
    
    
    
    실제로 O(n^2)으로 풀어봤을 때 시간초과가 나왔다. 이 문제는 접근 방식을 달리해 O(n)으로 풀어야한다.
    
    사실 한참 고민했다. O(n)으로 풀어야함은 반복문을 한번만 돌림으로써 문제가 해결되어야 하는데 도저히 떠오르지가 않았다.
    
    
    
    그러다 주식을 구매하고 바로 뒤의 가격이 올랐으면 팔 수 있고 떨어졌으면 팔지 못하는 방식을 이용해, 가격이 올랐으면 계속 반복문을 이어가며 (판 가격 - 산 가격)과 현재 저장되어있는 최대 이익을 비교해서 갱신해주고 만약 가격이 떨어졌으면 더 싸게 살 수 있으니 산 가격을 바꿈으로써 반복문을 이어나갔다. 위 과정을 차례로 반복하면 1번의 반복으로, 즉 O(n)으로 문제를 해결할 수 있을 것이라 생각했고 바로 코드 작업에 들어갔다.
    
    
    
    💻 제출 코드
    class Solution {
        public int maxProfit(int[] prices) {
            int profit = 0;
            int buy = prices[0];
    
            for(int i=1; i<prices.length; i++) {
                int sell = prices[i];
                if(buy < sell) {
                    profit = Math.max(profit, sell-buy);
                }
                else {
                    buy = sell;
                }
            }
    
            return profit;
        }
    }
    
    🌱 풀이 후기
    ﻿
    
    
    Runtime도 Memory도 잘 나온 것을 알 수 있다. 위 방식은 내가 생각하기에 '그리디 알고리즘'에 가깝다고 생각한다. 왜냐하면 눈 앞에 보이는 최대의 이익에 따라서 계산을 이어나가기 때문이다. 접근 방식에서 막혔을 때, 그리디 알고리즘을 꼭 떠올리고 단순하게 추상화 하는 습관을 들여보자.

---
+++ 121. Best Time to Buy and Sell Stock.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int profit = 0;
+        int buy = prices[0];
+
+        for(int i=1; i<prices.length; i++) {
+            int sell = prices[i];
+            if(buy < sell) {
+                profit = Math.max(profit, sell-buy);
+            }
+            else {
+                buy = sell;
+            }
+        }
+
+        return profit;
+    }
+}
