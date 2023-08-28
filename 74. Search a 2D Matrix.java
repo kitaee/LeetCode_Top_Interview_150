3d6b2d78bb49f0a13af74156a677e1ba6c2b153f
이기태 <91439717+kitaee@users.noreply.github.com>
2023-08-28 16:50:36 +0900

    Create 74. Search a 2D Matrix.java
    🔗 문제 링크
    https://leetcode.com/problems/linked-list-cycle/description/?envType=study-plan-v2&envId=top-interview-150 
    
    
    Linked List Cycle - LeetCode
    
    Can you solve this real interview question? Linked List Cycle - Given head, the head of a linked list, determine if the linked list has a cycle in it. There is a cycle in a linked list if there is some node in the list that can be reached again by continuo
    
    leetcode.com
    📝 문제 설명
    LinkedList head가 주어졌을 때 이 head가 순환되는지 아닌지 판별하는 문제이다.
    
    
    
    
    🤔 접근 방식
    사실 이런 유형의 문제는 처음이라서 한참 해맸다가 결국 다른 레퍼런스를 보고 해결했다.
    
    https://devraphy.tistory.com/554
    
    
    LeetCode 141(Linked List Cycle, java)
    
    0. 문제 https://leetcode.com/problems/linked-list-cycle/ Linked List Cycle - LeetCode Level up your coding skills and quickly land a job. This is the best place to expand your knowledge and get prepared for your next interview. leetcode.com 1. 문제 설
    
    devraphy.tistory.com
    위 블로그를 참고했고 문제 제약 조건의 중요성을 많이 깨달은 문제이다.
    
    
    
    문제 제약 조건을 보면 head의 val이 10^5보다 작거나 같은 값을 가지는데 이를 활용해야 한다. 
    
    보통 LinkedList 문제를 보면 시작점을 많이 알려주는 편인데 이 문제는 시작점을 가르쳐주지 않아 상당히 곤란한 문제 중 하나이다.
    
    
    
    그래서 모든 LinkedList의 값이 10^5보다 작은 점을 이용해서 탐색을 시작하면 탐색한 값들을 모두 10^5보다 큰 값으로 세팅해놓는 것이다. 그러다가 10^5보다 큰값을 만나면 이는 분명 우리가 임의로 세팅한 값이니 순환한다고 판단하면 되는 것이다.
    
    
    💻 제출 코드
    /**
     * Definition for singly-linked list.
     * class ListNode {
     *     int val;
     *     ListNode next;
     *     ListNode(int x) {
     *         val = x;
     *         next = null;
     *     }
     * }
     */
    
    public class Solution {
        public boolean hasCycle(ListNode head) {
            while(head != null) {
                if(head.val == 9999999) {
                    return true;
                }
    
                head.val = 9999999;
                head = head.next;
            }
    
            return false;
        }
    }
    
    
    🌱 풀이 후기
    
    색다른 유형의 문제를 풀어볼 수 있어 굉장히 좋았다.
    
    
    
    또 문제 제약 조건을 역으로 이용해 문제를 푸는 과정에서 하나의 스킬을 얻은 것 같아서 뿌듯했다.

---
+++ 74. Search a 2D Matrix.java
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        while(head != null) {
+            if(head.val == 9999999) {
+                return true;
+            }
+
+            head.val = 9999999;
+            head = head.next;
+        }
+
+        return false;
+    }
+}
